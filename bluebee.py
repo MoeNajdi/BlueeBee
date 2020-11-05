44c8f8012bc6bbddbe2947843f2cf546863b8966
MoeNajdi <m.najdi@hotmail.com>
2020-11-05 22:52:22 +0200

    making him move

--- bluebee.py
+++ bluebee.py
@@ -19,6 +19,31 @@ class BlueBee:
 
         # Store a decimal value for BlueBee's position.
         self.x = float(self.rect.x)
+        #print(self.x)
+        self.y = float(self.rect.y)
+        #print(self.y)
+        # Movement flags.
+        self.moving_left = False
+        self.moving_right = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update_movement(self):
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.bb_speed
+            #print("left : ",self.x)
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.bb_speed
+            #print("right : ", self.x)
+        self.rect.x = self.x
+
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.bb_speed
+            #print("up : ", self.y)
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.bb_speed
+            #print("down : ", self.y)
+        self.rect.y = self.y
 
     def blitme(self):
         """Draw BlueBee on the screen."""
